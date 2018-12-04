d5135506cda57a012038797b63f23c549a019fc3
Brenton Gunning <brentongunning@gmail.com>
2018-12-03 17:31:02 -0800

    Make wordlist public

--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -7,4 +7,4 @@ pub use self::extended_key::{
     derive_extended_key, ExtendedKey, ExtendedKeyType, HARDENED_KEY, MAINNET_PRIVATE_EXTENDED_KEY,
     MAINNET_PUBLIC_EXTENDED_KEY, TESTNET_PRIVATE_EXTENDED_KEY, TESTNET_PUBLIC_EXTENDED_KEY,
 };
-pub use self::mnemonic::{load_wordlist, mnemonic_decode, mnemonic_encode};
+pub use self::mnemonic::{load_wordlist, mnemonic_decode, mnemonic_encode, Wordlist};
